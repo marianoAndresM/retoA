19c83677b1c2d2fec1084157d2a2d41fb696883e
Mariano Andrés Messana Montenegro <mmessana@rfranco.com>
2022-03-31 12:20:12 +0200

    3

--- src/boton.js
+++ src/boton.js
@@ -4,16 +4,21 @@ export default class Boton extends Phaser.GameObjects.Container {
     this.funcionEjecutar = funcionEjecutar;
     this.texto = texto;
     
+    //botón
+    this.boton = this.scene.add.text(0, 180, this.texto, { font: '40px Verdana', fill: '#ffffff', fontSize: 45 }).setOrigin(0.5);
+    this.boton.depth = 1;
+    
+    //hitArea
+    const hitArea = new Phaser.Geom.Rectangle(-this.boton.width, 190 -this.boton.height, 2 * this.boton.width, 2*this.boton.height - 20);
+    
     //forma
     this.forma = this.scene.add.graphics();
-    this.forma.lineStyle(2, 0x000000, 1);
-    this.forma.strokeRoundedRect(-80, 120, 160, 60, 32);
-    this.forma.lineStyle(4, 0xff00ff, 1);
+    this.forma.fillStyle(0x000000, 0.7);
+    this.forma.fillRoundedRect(-this.boton.width, 180 -this.boton.height, 2 * this.boton.width, 2*this.boton.height, 20);
+    // this.forma.lineStyle(4, 0xff00ff, 1);
+    this.forma.setInteractive(hitArea, Phaser.Geom.Rectangle.Contains);
+    this.forma.on('pointerdown', this.funcionEjecutar, this.scene);
     this.add(this.forma)
-    
-    //botón
-    this.boton = this.scene.add.text(0, 155, this.texto, { font: '40px Verdana', fill: '#000', fontSize: 45 }).setOrigin(0.5).setInteractive({ useHandCursor: true });
-    this.boton.on('pointerdown', this.funcionEjecutar, this.scene);
     this.add(this.boton);
     
     this.scene.add.existing(this);
