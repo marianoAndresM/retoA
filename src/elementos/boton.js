af921e661b3b3bfba42637b28b2fd4cd7fb8dda8
Mariano Andrés <mariano.a.messana@gmail.com>
2022-04-04 10:20:17 +0200

    5

--- src/elementos/boton.js
+++ src/elementos/boton.js
@@ -8,7 +8,7 @@ export default class Boton extends Phaser.GameObjects.Container {
     this.boton = this.scene.add.text(0, 180, this.texto, { 
       fontSize: 40,
       fontFamily: 'fuente',
-      fill: '#fff'
+      fill: '#ddd'
      }).setOrigin(0.5);
     this.boton.depth = 1;
     
@@ -19,8 +19,13 @@ export default class Boton extends Phaser.GameObjects.Container {
     this.forma = this.scene.add.graphics();
     this.forma.fillStyle(0x000000, 0.7);
     this.forma.fillRoundedRect(-this.boton.width, 180 -this.boton.height, 2 * this.boton.width, 2*this.boton.height, 20);
-    // this.forma.lineStyle(4, 0xff00ff, 1);
     this.forma.setInteractive(hitArea, Phaser.Geom.Rectangle.Contains);
+    this.forma.on('pointerover', function() {
+      this.boton.setColor('#fff');
+    }, this);
+    this.forma.on('pointerout', function() {
+      this.boton.setColor('#ddd');
+    }, this);
     this.forma.on('pointerdown', this.funcionEjecutar, this.scene);
     this.add(this.forma)
     this.add(this.boton);
--- src/escenas/menu.js
+++ src/escenas/menu.js
@@ -12,7 +12,17 @@ export default class Menu extends Phaser.Scene {
   create() {
 
     this.boton = new Boton(this, this.sys.game.config.width/2, 0, this.avanzar, 'Jugar');
-    console.log(this.boton);
+
+    const instrucciones = ["Pulse el botón Jugar para jugar",
+    "El triángulo da dos premios.",
+    "El círculo da un premio.",
+    "El rombo y el cuadrado dan cero premios."]
+
+    this.texto = this.add.text(0, 300, instrucciones, {font: '25px fuente', align: 'center'})
+    
+    const contenedor = this.add.container((this.sys.game.config.width - this.texto.width)/2, 0, this.texto); 
+
+    this.add.existing(contenedor);
 
   }
 
